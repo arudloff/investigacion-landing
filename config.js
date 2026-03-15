43b986c6a986f020bdb3303b20f06f0ba0b8bdcb
arudloff <alejandro@colegiocamilohenriquez.cl>
2026-03-15 15:10:50 -0300

    feat: zona privada con solicitud de acceso, acuerdo de confidencialidad y auth Supabase

---
+++ config.js
@@ -0,0 +1,3 @@
+// Supabase - claves públicas (publishable)
+var SUPABASE_URL = 'https://cupykpcsxjihnzwyflbm.supabase.co';
+var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1cHlrcGNzeGppaG56d3lmbGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTU1MjMsImV4cCI6MjA4OTE3MTUyM30.GFEW-prBl39zRGbqIhOfqKoWcVLICEIXXQvPkL9UaOU';
